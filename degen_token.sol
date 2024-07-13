1bbe14206b2567c1957038b3a447ceed21eef568
darkworldchamp <140893545+darkworldchamp@users.noreply.github.com>
2024-07-13 12:44:49 +0530

    Update degen_token.sol

--- degen_token.sol
+++ degen_token.sol
@@ -5,11 +5,11 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract DegenToken is ERC20, Ownable, ERC20Burnable {
+contract DegenGaming is ERC20, Ownable, ERC20Burnable {
     
     constructor() ERC20("Degen", "DGN") Ownable(msg.sender){}
     
-    // Enumeration representing different levels of item rarity
+    // Enumeration representing different levels of item rarity 
     enum Rarity {Classic, Special, Epic, Mythic, Legendary}
 
     // Structure representing an item with name, rarity, and price
@@ -22,8 +22,12 @@ contract DegenToken is ERC20, Ownable, ERC20Burnable {
     // Mapping to store items identified by a unique uint256 ID
     mapping(uint256 => Item) public items;
 
+    // Mapping to track redeemed items for each account
+    mapping(address => uint256[]) public redeemedItems;
+
     // Function to mint new tokens, only callable by the owner
     function mint(address to, uint256 amount) public onlyOwner {
+        require(amount > 0, "Amount must be greater than 0");
         _mint(to, amount);
     }
 
@@ -40,5 +44,18 @@ contract DegenToken is ERC20, Ownable, ERC20Burnable {
         require(bytes(item.name).length > 0, "Item does not exist");
         require(balanceOf(msg.sender) >= item.price, "Insufficient balance");
         _burn(msg.sender, item.price);
+
+        // Add the item to the redeemed items for the user
+        redeemedItems[msg.sender].push(itemId);
+    }
+
+    // Function to get the redeemed items for a specific account
+    function getRedeemedItems(address account) public view returns (Item[] memory) {
+        uint256[] memory itemIds = redeemedItems[account];
+        Item[] memory userItems = new Item[](itemIds.length);
+        for (uint256 i = 0; i < itemIds.length; i++) {
+            userItems[i] = items[itemIds[i]];
+        }
+        return userItems;
     }
 }
